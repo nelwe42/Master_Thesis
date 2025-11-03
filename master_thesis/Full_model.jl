1ce111a470b44de48e470cd607d75a5a7a23b604
nelwe42 <nelewellesen@web.de>
2025-11-03 14:58:18 +0100

    Added files for remaining model parts and renamed old ones

--- master_thesis/Full_model.jl
+++ master_thesis/Full_model.jl
@@ -5,7 +5,7 @@ struct FullModel
     #potentially split measurement and PK model?
     pk_model::PK_Model
     seizure_model::Seizure_Model
-    population_gen::Generator
+    population_gen::Person_Generator
     dose_gen::Dose_Generator
 end
 
@@ -39,8 +39,8 @@ function get_individual_likelihood(m::FullModel, data::Tuple)
     covariates = data[1].covariates
     #for each model write unpacker for covariates returning correct choice of covariates
     #in PK_model, Seizure_model save required covariates as list of keys
-    covariates_PK = NamedTuple{m.pk_model.covariates}(covariates)
-    covariates_Seizure = NamedTuple{m.seizure_model.covariates}(covariates)
+    covariates_PK = NamedTuple{m.pk_model.cov}(covariates)
+    covariates_Seizure = NamedTuple{m.seizure_model.cov}(covariates)
     #Is this well-behaved if covariate list empty?
     function likelihood(Theta)
         #unpack Theta somehow into two named tuples
