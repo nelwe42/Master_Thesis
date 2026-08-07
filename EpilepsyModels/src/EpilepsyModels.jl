fe81ab2861a2c112f4469d9ca60a02ddbb3c230c
nelwe42 <nelewellesen@web.de>
2026-08-07 11:54:03 +0200

    Removed test file

--- EpilepsyModels/src/EpilepsyModels.jl
+++ EpilepsyModels/src/EpilepsyModels.jl
@@ -17,7 +17,7 @@ using MCMCChains
 
 export optimise, optimise_hierarchical, optimise_sampled, generate_data, generate_data_updating, generate_data_modified, get_negloglikelihood_evaluated, get_negloglikelihood_evaluated_hierarchical, plot_fit, multi_data_run,
 BasicDoses, PolyDosesRandom, PolyDoses, BigFourDoses, PKBasic, PKLEV, PKLEVNoAbsorption, PKCBZ, PKVPA, PKLTG, PKBigFour,
-BasicPersonGenerator, PersonGeneratorLEV, BigFourPersonGenerator, SeizureBasic, SeizureNegativeBinomial, SeizureVPA, SeizureMult, SeizureSANAD, FullModel
+BasicPersonGenerator, PersonGeneratorLEV, BigFourPersonGenerator, SeizureBasic, SeizureNegativeBinomial, SeizureVPA, SeizureMult, SeizureSANAD, FullModel, Person
 
 include("Person Generator.jl")
 include("PK Model.jl")
