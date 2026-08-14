dc1efd292ddcbe6146f8dd7f5a70369c6c1d2319
nelwe42 <nelewellesen@web.de>
2026-08-14 19:19:37 +0200

    Wrong model done, sampling still running

--- EpilepsyModels/src/MultiData.jl
+++ EpilepsyModels/src/MultiData.jl
@@ -63,7 +63,7 @@ Population_size = 20 #5 #20 #10 #20
 wo_treatment = 0.0 #10.0
 Obs_Duration = wo_treatment + 20.0
 PK_timepoints = wo_treatment:3.75:Obs_Duration
-if parsed2 in [2,3]
+if parsed2 in [1,2]
     Seizure_timepoints = 0.0:1.0:Obs_Duration
 else
     Seizure_timepoints = 0.0:5.0:Obs_Duration
@@ -73,11 +73,8 @@ update_reg = Obs_Duration
 #logscale = ("σ",)
 #logscale = ("σ","v1")
 #logscale = ("σ", "k_abs", "c1", "v1", "a")
-if parsed2 in [1,3]
-    logscale = ("σ", "k_abs", "c1", "v1")
-else
-    logscale = ("σ_LEV", "k_abs_LEV", "c1_LEV", "v1_LEV", "σ_LTG", "k_abs_LTG", "c1_LTG", "v1_LTG","σ_CBZ", "k_abs_CBZ", "c1_CBZ", "v1_CBZ", "σ_VPA", "k_abs_VPA", "c1_VPA", "v1_VPA", "a")
-end
+logscale = ("σ", "k_abs", "c1", "v1")
+#logscale = ("σ_LEV", "k_abs_LEV", "c1_LEV", "v1_LEV", "σ_LTG", "k_abs_LTG", "c1_LTG", "v1_LTG","σ_CBZ", "k_abs_CBZ", "c1_CBZ", "v1_CBZ", "σ_VPA", "k_abs_VPA", "c1_VPA", "v1_VPA", "a")
 #logscale = ("σ", "k_abs", "c1", "c3", "v1", "a") 
 #logscale = ("σ", "c1", "v1", "a")
 solver_optim = LBFGS(linesearch = LineSearches.BackTracking())
@@ -110,7 +107,7 @@ sandwich = true
 finite_diff_hessian = false
 drug_appropriate_dosing = true
 hierarchical_optimisation = false
-sampling = true
+sampling = false
 plotting = false
 show_original = true
 
@@ -124,20 +121,18 @@ lock_path_output = joinpath("./", file_name * ".lock")
 =#
 
 #pk_model = PKBasic(θ=Input_θ_PKBasic)
-if parsed2 == 3
+if parsed2 in [1,2]
     pk_model = PKLEV(θ=Input_θ_PKLEV)
 end
 #pk_model = PKLEVNoAbsorption(θ=Input_θ_PKLEVNoAbsorption)
 #pk_model = PKCBZ(θ=Input_θ_PKCBZ)
-if parsed2 == 1
+if parsed2 == 3
     pk_model = PKVPA(θ=Input_θ_PKVPA)
 end
 #pk_model = PKLTG(θ=Input_θ_PKLTG)
-if parsed2 == 2
-    pk_model = PKBigFour(θ = Input_θ_PKBigFour)
-end
+#pk_model = PKBigFour(θ = Input_θ_PKBigFour)
 #Set b in seizure_basic according to pk model (different daily exposures), for VPA 0.2 is too high
-if parsed2 == 2
+if parsed2 == 1
     if (typeof(pk_model).name.wrapper in [PKVPA])
         Input_θ_SeizureBasic_one.b = SA[0.05]
     end
@@ -157,17 +152,41 @@ if parsed2 == 2
             end
         end
     end
+elseif parsed2 in [2,3]
+    if (typeof(pk_model).name.wrapper in [PKVPA])
+        Input_θ_SeizureBasic_one.b = SA[0.05]
+    end
+    if (typeof(pk_model).name.wrapper in [PKBigFour])
+        seizure_model2 = SeizureBasic(θ = Input_θ_SeizureBasic_four)
+    else
+        seizure_model2 = SeizureBasic(θ = Input_θ_SeizureBasic_one)
+        if drug_appropriate_dosing
+            if (typeof(pk_model).name.wrapper in [PKLEV, PKLEVNoAbsorption])
+                Input_θ_SeizureBasic_one.b = SA[Input_θ_SeizureBasic_four.b[2]]
+            elseif (typeof(pk_model).name.wrapper in [PKLTG])
+                Input_θ_SeizureBasic_one.b = SA[Input_θ_SeizureBasic_four.b[1]]
+            elseif (typeof(pk_model).name.wrapper in [PKCBZ])
+                Input_θ_SeizureBasic_one.b = SA[Input_θ_SeizureBasic_four.b[3]]
+            elseif (typeof(pk_model).name.wrapper in [PKVPA])
+                Input_θ_SeizureBasic_one.b = SA[Input_θ_SeizureBasic_four.b[4]]
+            end
+        end
+    end
 end
 #seizure_model = SeizureMult(pk_model, base_rate = base_rate, default_treat_eff = 0.2)
-if parsed2 == 1
+if parsed2 == 3
     seizure_model = SeizureVPA(θ = Input_θ_SeizureVPA)
 end
 if pk_model isa PKVPA
     Input_θ_SeizureNegativeBinomial.b[1] = 0.05
 end
-#seizure_model = SeizureNegativeBinomial(θ = Input_θ_SeizureNegativeBinomial)
+if parsed2 == 2
+    seizure_model = SeizureNegativeBinomial(θ = Input_θ_SeizureNegativeBinomial)
+elseif parsed2 == 1
+    seizure_model2 = SeizureNegativeBinomial(θ = Input_θ_SeizureNegativeBinomial)
+end
 #SeizureSANAD set b appropriately
-if parsed2 == 3
+#=
     if (typeof(pk_model).name.wrapper in [PKBigFour])
         seizure_model = SeizureSANAD(θ = Input_θ_SeizureSANAD_four, baseline = base_rate)
     else
@@ -184,7 +203,7 @@ if parsed2 == 3
         end
         seizure_model = SeizureSANAD(θ = Input_θ_SeizureSANAD_one, baseline = base_rate)
     end
-end
+=#
 person_gen = BigFourPersonGenerator()
 #dose_gen = BasicDoses(default_dose=500.0, times_per_day=2)
 #dose_gen = PolyDoses(pk_model, default_dose=500.0)
@@ -199,20 +218,34 @@ if seizure_model isa SeizureVPA && !(dose_gen isa BigFourDoses)
 elseif seizure_model isa SeizureVPA
     dose_gen = BigFourDoses(order_male = ((:d_VPA,:d_CBZ), (:d_VPA,:d_CBZ)), order_female = ((:d_VPA,:d_CBZ), (:d_VPA,:d_CBZ)), prob_second = 0.5, prob_reassignment = 0.0)
 end
-Input_θ = ComponentArray(PK = pk_model.θ, Seizure = seizure_model.θ)
+#For evaluation put some guess as dummy
+Input_θ = ComponentArray(PK = pk_model.θ, Seizure = seizure_model2.θ)
 mod = FullModel(pk_model, seizure_model, person_gen, dose_gen)
+mod2 = FullModel(pk_model, seizure_model2, person_gen, dose_gen)
 
 
 #This will redirect output to txt file, not including error messages/warnings
 path = "/home/s6newell_hpc"
 #path = "."
-file_name = "Sampling_$(typeof(pk_model).name.wrapper).txt"
+file_name = "WrongModel_$(typeof(seizure_model).name.wrapper)_to_$(typeof(seizure_model2).name.wrapper).txt"
 lock_path_output = joinpath("./", file_name * ".lock")
 
 
 modifications = ()
 #Set data generating function
-data() = generate_data(mod, Population_size, Obs_Duration, timepoints_PK = PK_timepoints, timepoints_seizure = Seizure_timepoints, wo_treatment = wo_treatment, max_threads = max_threads_simul, just_Bool = no_counts_seizure, ODE_options = ODE_options)
+if parsed2 in [1,2]
+    data() = generate_data(mod, Population_size, Obs_Duration, timepoints_PK = PK_timepoints, timepoints_seizure = Seizure_timepoints, wo_treatment = wo_treatment, max_threads = max_threads_simul, just_Bool = no_counts_seizure, ODE_options = ODE_options)
+elseif parsed2 == 3
+    #Somewhat handle seizurevpa records success and seizurebasic records failures
+    invert_seizures(x::NamedTuple) = (time = x.time, count = !x.count)
+    function data()
+        dataset = generate_data(mod, Population_size, Obs_Duration, timepoints_PK = PK_timepoints, timepoints_seizure = Seizure_timepoints, wo_treatment = wo_treatment, max_threads = max_threads_simul, just_Bool = no_counts_seizure, ODE_options = ODE_options)
+        for person in dataset
+            person.seizure_counts .= invert_seizures.(person.seizure_counts)
+        end
+        return dataset
+    end
+end
 data_modified() = generate_data_modified(mod, Population_size, Obs_Duration, update_reg = update_reg, modifications = modifications, timepoints_PK = PK_timepoints, timepoints_seizure = Seizure_timepoints, max_threads = max_threads_simul, just_Bool = no_counts_seizure, wo_treatment = wo_treatment, ODE_options = ODE_options)
 data_updating() = generate_data_updating(mod, Population_size, Obs_Duration, update_reg = update_reg, timepoints_PK = PK_timepoints, timepoints_seizure = Seizure_timepoints, max_threads = max_threads_simul, just_Bool = no_counts_seizure, wo_treatment = wo_treatment, ODE_options = ODE_options)
 
@@ -250,26 +283,26 @@ if hierarchical_optimisation
                         bound_abs = bound_abs, lower_upper = lower_upper_bounds, objective_fail_hard=fail_hard, store_trace = false,
                         printing = false, solver_optim = solver_optim, ODE_options = ODE_options, 
                         run_CI = run_CI, confidence = confidence, finite_not_forward = finite_diff_hessian, sandwich = sandwich)
-    eval(data) = get_negloglikelihood_evaluated_hierarchical(Input_θ, mod, data, logscale = logscale, ODE_options = ODE_options)
+    eval(data) = get_negloglikelihood_evaluated_hierarchical(Input_θ, mod2, data, logscale = logscale, ODE_options = ODE_options)
 elseif sampling
     estimate(new_mod, data) = optimise_sampled(deepcopy(new_mod), data, per_chain=Samples_per_chain, nadapts = Adaptation_steps, 
                         bound_abs = bound_abs, lower_upper = lower_upper_bounds, objective_fail_hard=fail_hard, multistart = Multistart_nstarts, 
                         prefilter = prefilter, custom_starts = custom_starts, max_threads = max_threads_simul, multistart_seed = Multistart_seed, 
                         multistart_include_initial = Multistart_include_initial, multistart_bounds = Multistart_bounds, printing = false, 
                         run_CI = run_CI, confidence = confidence, sampler = sampler, sampling_options = sampling_options) 
-    eval(data) = get_negloglikelihood_evaluated(Input_θ, mod, data, logscale = logscale, max_threads = max_threads_simul, ODE_options = ODE_options)
+    eval(data) = get_negloglikelihood_evaluated(Input_θ, mod2, data, logscale = logscale, max_threads = max_threads_simul, ODE_options = ODE_options)
 else
     estimate(new_mod, data) = optimise(new_mod, data, maxiters = Maxiters_optimiser, maxtime = Max_Time, logscale = logscale, solver_optim = solver_optim, ODE_options = ODE_options,
         bound_abs = bound_abs, lower_upper = lower_upper_bounds, objective_fail_hard=fail_hard, store_trace = false,
         multistart = Multistart_nstarts, prefilter = prefilter, custom_starts = custom_starts, max_threads = max_threads_simul, multistart_seed = Multistart_seed,
         multistart_include_initial = Multistart_include_initial, multistart_bounds = Multistart_bounds, printing = false,
         run_CI = run_CI, confidence = confidence, finite_not_forward = finite_diff_hessian, sandwich = sandwich)
-    eval(data) = get_negloglikelihood_evaluated(Input_θ, mod, data, logscale = logscale, max_threads = max_threads_simul, ODE_options = ODE_options)
+    eval(data) = get_negloglikelihood_evaluated(Input_θ, mod2, data, logscale = logscale, max_threads = max_threads_simul, ODE_options = ODE_options)
 end
 
 try
 
-results = multi_data_run(mod, data, estimate, eval, run_count=run_count, max_threads_runs=max_threads_runs)
+results = multi_data_run(mod2, data, estimate, eval, run_count=run_count, max_threads_runs=max_threads_runs)
 
 
 #create lockfile to ensure no multiwriting
