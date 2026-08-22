b09be8997ad608ab249866160ca002e84b83a95d
nelwe42 <nelewellesen@web.de>
2026-08-22 11:05:46 +0200

    Replotted Update Reg

--- EpilepsyModels/src/Plotting_script.jl
+++ EpilepsyModels/src/Plotting_script.jl
@@ -6,33 +6,31 @@ using Random
 
 #save figures after running?
 saving = true
-save_path = "./SeizureFreq"
+save_path = "./UpdateReg"
 
 #files to read data from, relative to 
-files = [["./SeizureFreq/SeizureFreq_Basic_1_false.txt", "./SeizureFreq/SeizureFreq_Basic_2_false.txt", "./SeizureFreq/SeizureFreq_Basic_5_false.txt", "./SeizureFreq/SeizureFreq_Basic_10_false.txt"], 
-        ["./SeizureFreq/SeizureFreq_NB_1_false.txt", "./SeizureFreq/SeizureFreq_NB_2_false.txt", "./SeizureFreq/SeizureFreq_NB_5_false.txt", "./SeizureFreq/SeizureFreq_NB_10_false.txt"], 
+files = [["./UpdateReg/UpdateReg_Basic_1.txt", "./UpdateReg/UpdateReg_Basic_2.txt", "./UpdateReg/UpdateReg_Basic_5.txt", "./UpdateReg/UpdateReg_Basic_15.txt", "./UpdateReg/UpdateReg_Basic_30.txt"],
+        ["./UpdateReg/UpdateReg_VPA_1.txt", "./UpdateReg/UpdateReg_VPA_2.txt", "./UpdateReg/UpdateReg_VPA_5.txt", "./UpdateReg/UpdateReg_VPA_15.txt", "./UpdateReg/UpdateReg_VPA_30.txt"],
+        ["./UpdateReg/UpdateReg_SANAD_1.txt", "./UpdateReg/UpdateReg_SANAD_2.txt", "./UpdateReg/UpdateReg_SANAD_5.txt", "./UpdateReg/UpdateReg_SANAD_15.txt", "./UpdateReg/UpdateReg_SANAD_30.txt"]
         ]
-files2 = [["./SeizureFreq/SeizureFreq_Basic_1_true.txt", "./SeizureFreq/SeizureFreq_Basic_2_true.txt", "./SeizureFreq/SeizureFreq_Basic_5_true.txt", "./SeizureFreq/SeizureFreq_Basic_10_true.txt"], 
-        ["./SeizureFreq/SeizureFreq_NB_1_true.txt", "./SeizureFreq/SeizureFreq_NB_2_true.txt", "./SeizureFreq/SeizureFreq_NB_5_true.txt", "./SeizureFreq/SeizureFreq_NB_10_true.txt"], 
-        ["./SeizureFreq/SeizureFreq_VPA_5.txt", "./SeizureFreq/SeizureFreq_VPA_10.txt", "./SeizureFreq/SeizureFreq_VPA_15.txt", "./SeizureFreq/SeizureFreq_VPA_20.txt"], 
-        ]
+files2 = []
 #Do space before name if not empty, else empty string
-names_distinction = (" seizure counts", " just Bool")
+names_distinction = ("", " just Bool")
 #models each subarray corresponds to
-models = ["Basic", "Negative Binomial", "VPA"]
+models = ["Basic", "VPA", "SANAD"]
 #colour for each model
-colours = [[:teal, :violet], [:cyan, :fuchsia, :orange]]
+colours = [[:teal, :orange, :brown], [:cyan, :fuchsia, :orange]]
 #quantity of interest
-quant = "seizure measurement frequency"
-short_quant = "Seizure_Freq"
+quant = "update regularity"
+short_quant = "UpdateReg"
 #corresponding values of interest for each model
-values = [[1,2,5,10], [1,2,5,10],[5,10,15,20]]
+values = [[1,2,5,15,30] for model in models]
 #Legend setting for plotting
 legendcolumns = isempty(names_distinction[1]) || isempty(names_distinction[2]) ? 2 : 1
 #set variable of interest below
 
-upper_plotting_bound = [2.0, 40.0, 50.0]
-upper_outlier_bound = [150.0, 300.0, 500.0]
+upper_plotting_bound = [500.0, 50.0, 50.0]
+upper_outlier_bound = [500.0 for model in models]
 spaced_accordingly = false
 plot_separate = true
 
